3e28ed8bea1d09226475476aa2fe8d2c2f386c55
Keshav Saraogi <tuj49866@temple.edu>
2024-06-26 10:47:29 +0530

    components
    -- navbar
    -- footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,13 @@
+import Link from 'next/link'
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './navbar.modules.css'
+
+const Navbar = () => {
+    return (
+        <div className="">Navbar</div>
+    )
+}
+
+export default Navbar
