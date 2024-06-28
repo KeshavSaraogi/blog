be7c1482f1ba1cf41c3fdc3b61c834f812797c97
Keshav Saraogi <tuj49866@temple.edu>
2024-06-28 14:00:56 +0530

    components
    -- categoryList
    -- Card
    -- CardList

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './card.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.imageContainer}>
+            <Image src="/view.jpeg" alt="" fill/>
+        </div>
+        <div className={styles.textContainer}>
+            <div className={styles.detail}>
+                <span className={styles.date}>11.02.2023 - </span>
+                <span className={styles.category}>CULTURE</span>
+            </div>
+            <Link href="/">
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+            </Link>
+            <p className={styles.desc}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Neque molestiae laboriosam non ullam nulla. 
+                Sint asperiores dicta totam beatae, mollitia laudantium nisi iste laborum molestiae illo voluptas similique ratione officiis!
+            </p>
+            <Link href="/" className={styles.link}>Read More</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Pagination from '@/components/pagination/Pagination'
 import styles from './cardList.module.css'
+import Image from 'next/image'
+import Card from '../card/Card'
 
 const CardList = () => {
     return (
-        <div className={styles.container}>CardList
+        <div className={styles.container}>
+            <h1 className={styles.title}>Recent Post</h1>
+            <div className={styles.posts}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
             <Pagination />
         </div>
     )
