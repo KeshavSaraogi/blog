1a28ac659b76f6b388596bd1baa4bea2d7231116
Keshav Saraogi <tuj49866@temple.edu>
2024-06-30 13:20:46 +0530

    UI
    -- page component

--- src/app/write/Page.jsx
+++ src/app/write/Page.jsx
@@ -1,8 +1,14 @@
+"use client"
+
 import React from 'react'
 import styles from "./writePage.module.css"
+import {useState} from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.bubble.css'
 
 const Page = () => {
   const [open, setpen] = useState(false)
+  const [value, setValue] = useState("")
 
   return (
     <div className={styles.container}>
@@ -11,7 +17,7 @@ const Page = () => {
             <button className={styles.button}>
                 <Image src="./plus.png" alt="" width={16} height={16}/>
             </button>
-            {open && 
+            {open && (
             <div className={styles.add}>
                 <button className={styles.addButton}>
                     <Image src="./image.png" alt="" width={16} height={16}/>
@@ -23,8 +29,16 @@ const Page = () => {
                     <Image src="./video.png" alt="" width={16} height={16}/>
                 </button>
             </div>
-            }
+            )}
+            <ReactQuill 
+                className={styles.textArea} 
+                theme="bubble" 
+                value={value} 
+                onChange={setValue} 
+                placeholder="Tell Your Story..."
+            />
         </div>
+        <button className={styles.publish}>Publish</button>
     </div>
   )
 }
