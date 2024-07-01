94187c4343d449c75a60bfc5db65847a182438d8
Keshav Saraogi <tuj49866@temple.edu>
2024-07-01 14:17:57 +0530

    UI
    -- completing the UI

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -15,6 +15,7 @@ const AuthLinks = () => {
       ) : (
         <>
           <Link href="/write">Write</Link>
+          <span className={styles.link} onClick={signOut}>Logout</span>
           <span className={styles.link}>Login</span>
         </>
       )}
