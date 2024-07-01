08574110a58510af794f3b92e0965f602730b746
Keshav Saraogi <tuj49866@temple.edu>
2024-07-01 08:47:35 +0530

    auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,33 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '../components/navbar/Navbar'
-import Footer from '../components/footer/Footer'
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import AuthProvider from '@/providers/AuthProvider'
+import { ThemeContextProvider } from "@/context/ThemeContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Keshav Saraogi Blog App",
+  description: "Personal Blog App!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container">
-          <div className="wrapper">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
-        </div>
+        <AuthProvider>
+          <ThemeContextProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                    {children}
+                  <Footer />
+                </div>
+              </div>
+          </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
-  )
+  );
 }
