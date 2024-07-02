38431331225e7c7761c43d181b264832e974625e
Keshav Saraogi <tuj49866@temple.edu>
2024-07-02 18:02:14 +0530

    database response
    -- integrating response from the database

--- src/app/write/Page.jsx
+++ src/app/write/Page.jsx
@@ -4,11 +4,25 @@ import React from 'react'
 import styles from "./writePage.module.css"
 import {useState} from 'react'
 import ReactQuill from 'react-quill'
+import { useSession } from 'next-auth/react'
+
 import 'react-quill/dist/quill.bubble.css'
+import { useRouter } from 'next/router'
 
 const Page = () => {
-  const [open, setOpen] = useState(false)
-  const [value, setValue] = useState("")
+    const { status } = useSession();
+    const router = useRouter();
+
+    
+    const [open, setOpen] = useState(false)
+    const [value, setValue] = useState("")
+    if (status === "loading") {
+        return <div className={styles.loading}>Loading...</div>;
+    }
+    
+    if (status === "authenticated") {
+        router.push("/")
+    }
 
   return (
     <div className={styles.container}>
