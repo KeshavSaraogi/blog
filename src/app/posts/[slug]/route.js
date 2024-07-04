b477ad59c45995628abe97796d8accd1089ad66f
Keshav Saraogi <tuj49866@temple.edu>
2024-07-04 12:47:55 +0530

    api
    -- comments
    -- singlePage

--- src/app/posts/[slug]/route.js
+++ src/app/posts/[slug]/route.js
@@ -1,21 +1,62 @@
-import prisma from "@/utils/connect";
-import { NextResponse } from "next/server";
+import Menu from "@/components/Menu/Menu";
+import styles from "./singlePage.module.css";
+import Image from "next/image";
+import Comments from "@/components/comments/Comments";
 
-export const GET = async (req, { params }) => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const SinglePage = async ({ params }) => {
   const { slug } = params;
 
-  try {
-    const post = await prisma.post.update({
-      where: { slug },
-      data: { views: { increment: 1 } },
-      include: { user: true },
-    });
+  const data = await getData(slug);
 
-    return new NextResponse(JSON.stringify(post, { status: 200 }));
-  } catch (err) {
-    console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-    );
-  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>{data?.title}</h1>
+          <div className={styles.user}>
+            {data?.user?.image && (
+              <div className={styles.userImageContainer}>
+                <Image src={data.user.image} alt="" fill className={styles.avatar} />
+              </div>
+            )}
+            <div className={styles.userTextContainer}>
+              <span className={styles.username}>{data?.user.name}</span>
+              <span className={styles.date}>01.01.2024</span>
+            </div>
+          </div>
+        </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data.img} alt="" fill className={styles.image} />
+          </div>
+        )}
+      </div>
+      <div className={styles.content}>
+        <div className={styles.post}>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
+          <div className={styles.comment}>
+            <Comments postSlug={slug}/>
+          </div>
+        </div>
+        <Menu />
+      </div>
+    </div>
+  );
 };
+
+export default SinglePage;
