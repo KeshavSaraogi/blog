fe3a0beab8ab819e8043d59d0062ecf82154d2d4
Keshav Saraogi <tuj49866@temple.edu>
2024-07-05 12:33:29 +0530

    some more changes
    -- next.js config additions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-      domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
-    }
-  }
-  
-module.exports = nextConfig
+  images: {
+    domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+  },
+  experimental: {
+    reactRoot: true,
+    serverComponents: true,
+  },
+};
+
+module.exports = nextConfig;
